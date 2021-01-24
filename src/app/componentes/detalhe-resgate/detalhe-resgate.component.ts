8000eb9b36844cf5e5b4f6e99a702f4960541313
odntht <odntht@gmail.com>
2021-01-24 11:37:02 -0300

    criando Modal de Informacao

--- src/app/componentes/detalhe-resgate/detalhe-resgate.component.ts
+++ src/app/componentes/detalhe-resgate/detalhe-resgate.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InvestimentosService } from 'src/app/services/investimentos.service';
+import { AppComponent } from "../../app.component";
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-detalhe-resgate',
@@ -10,11 +12,50 @@ export class DetalheResgateComponent implements OnInit {
 
   public investimentos: any = [];
 
-  constructor( private _investimentoService : InvestimentosService) { }
+  constructor(
+    private _investimentoService: InvestimentosService,
+    private _appComponent: AppComponent,
+    public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.investimentos = this._investimentoService.obterInvestimentos();
-    console.log(this.investimentos[0].response.data.listaInvestimentos);
+    
   }
 
+  retornarPagina() {
+    this._appComponent.selecionarPagina(1);
+  }
+
+  abrirModal() {
+    console.log('Modal Aberta!');
+    this.dialog.open(DialogElementsExampleDialog);
+  }
+
+}
+
+@Component({
+  selector: 'dialog-elements-example-dialog',
+  template: `
+  <div>
+    <div>
+      <h1>Resgate efetuado com sucesso!</h1>
+      <button (click)="fecharModal()">X</button>
+    </div>
+    <div>
+    <p>O valor solicitado será disponibilizado em até 5 dias úteis</p>
+    </div>
+    <button (click)="fecharModal()">Novo Resgate</button>
+  </div>
+  `
+})
+
+
+
+export class DialogElementsExampleDialog {
+
+  constructor( public dialog: MatDialog) { }
+
+  fecharModal() {
+    console.log('Fechando Modal...');
+    this.dialog.closeAll();
+  }
 }
